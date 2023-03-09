2839aaf18934d219c3a9f642869e660bccb4112c
CHENGZUDONG <chengzudong@gmail.com>
2023-03-09 18:17:22 +1100

    First commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int click = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        click++;
+        text.setText("You have clicked" + click+ "times.");
+    }
+}
